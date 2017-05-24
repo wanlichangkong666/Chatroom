0e49f2f6390bb977f5890bb9b5125b63eaa3f03d
王翔宇 <511649091@qq.com>
2017-05-24 21:09:21 +0800

    Chatroom

---
+++ Unity聊天室/Client.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Unity聊天室
+{
+    /// <summary>
+    /// 用来和客户端做通信
+    /// </summary>
+    class Client
+    {
+        private Socket clientSocket;
+        private Thread t;
+        private byte[] data = new byte[1024];//数据容器
+        public Client(Socket clientSocket)
+        {
+            this.clientSocket = clientSocket;
+            //启动一个线程，处理客户端的数据接收
+             t = new Thread(ReceiveMessage);
+            t.Start();
+        }
+        void ReceiveMessage()
+        {
+            while(true)
+            {
+                if(clientSocket.Poll(10,SelectMode.SelectRead))
+                {
+                    clientSocket.Close();
+                    break;//跳出循环，终止线程
+                }
+                int length = clientSocket.Receive(data);
+                string message = Encoding.UTF8.GetString(data, 0, length);
+                //将收到的消息广播到各个客户端
+                Program.BroadcastMessage(message);
+                Console.WriteLine(message);
+            }
+        }
+        public void SendMessage(string message)
+        {
+            byte[] data2 = Encoding.UTF8.GetBytes(message);
+            clientSocket.Send(data2);
+        }
+        public bool Connected
+        {
+            get { return clientSocket.Connected; }
+        }
+       
+    }
+}
---
+++ Unity聊天室/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unity聊天室
+{
+    class Program
+    {
+        static List<Client> clientList = new List<Client>();
+        /// <summary>
+        /// 广播消息
+        /// </summary>
+        /// <param name="message"></param>
+       public  static void BroadcastMessage(string message)
+        {
+            List<Client> notConnectedList = new List<Client>();
+            foreach(Client client in clientList)
+            {
+                if(client.Connected)
+                {
+                    client.SendMessage(message);
+                }            
+                else
+                {
+                    notConnectedList.Add(client);
+                }
+            }
+            foreach (var client in notConnectedList)
+            {
+                clientList.Remove(client);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Socket tcpServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ip =IPAddress.Parse("10.130.133.61");
+            EndPoint point = new IPEndPoint(ip, 7788);
+            tcpServer.Bind(point);
+            tcpServer.Listen(100);
+            Console.WriteLine("server is running");
+            while(true)
+            {
+                Socket clientSocket = tcpServer.Accept();
+                Client client = new Client(clientSocket);
+                clientList.Add(client);
+                Console.WriteLine("a client is connected!");
+            }
+            
+
+        }
+    }
+}
